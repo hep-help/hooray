f9f469edc176115d2119d98e07a3f8ad670d5032
Jim Pivarski <jpivarski@gmail.com>
2024-03-04 21:26:13 -0600

    set up response to new discussions 14

--- scripts/comment-on-discussion.py
+++ scripts/comment-on-discussion.py
@@ -58,7 +58,7 @@ payload = {
     }
 }""" % DISCUSSION_ID
 }
-print(json.dumpss(payload, indent=4))
+print(json.dumps(payload, indent=4))
 
 response = requests.post("https://api.github.com/graphql", json=payload)
 print(f"{response.status_code = }")
